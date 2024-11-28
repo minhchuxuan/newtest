2f51d7e205bf1420dbd07dc9c82d864b80b93679
minhchuxuan <64071806+minhchuxuan@users.noreply.github.com>
2024-11-28 08:25:16 +0700

    Update Cart.java

--- AimsProject/src/Cart.java
+++ AimsProject/src/Cart.java
@@ -11,6 +11,22 @@ public class Cart {
             System.out.println(String.format("Disc %d has been added", qtyOrdered));
         }
     }
+    public void addDigitalVideoDisc(DigitalVideoDisc [] dvdList) {
+        if (dvdList.length > MAX_NUMBERS_ORDERED) {
+            System.out.println("The cart is almost full!");
+        } else {
+            for (int i = 0; i < dvdList.length; i++) {
+                itemsOrdered[qtyOrdered] = dvdList[i];
+                System.out.println(dvdList[i].getTitle() + " has been added!");
+                qtyOrdered +=1 ;
+            }
+    
+        }
+    }
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1,DigitalVideoDisc dvd2) {
+        DigitalVideoDisc [] dvdList = {dvd1, dvd2};
+        addDigitalVideoDisc(dvdList);
+    }
     public void removeDigitalVideoDisc(DigitalVideoDisc disc){
         if (qtyOrdered == 0){
             System.out.println("There is no disk in the cart");
